4e1d638a2d6def459530a4bc1f3aba3f481f3ff1
mainor martinez <martimai000@warren.k12.in.us>
2024-10-25 10:09:49 -0400

    initial

---
+++ main.py
@@ -0,0 +1,10 @@
+age = int(input("Enter your age: "))
+
+if age < 5:
+    print ("You should play with toys")
+elif age >= 5 and age <= 12:
+    print ("You can play outdoor games")
+elif age >= 13 and age <= 17:
+    print ("You might enjoy video games")
+else : age <= 18
+print ("You can try hiking")
